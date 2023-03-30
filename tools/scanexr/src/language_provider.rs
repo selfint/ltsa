ebd3bb7f0ad266d9c3564ddd36e387411de6a60f
selfint <selfint@gmail.com>
2023-03-30 20:46:54 +0300

    re-implement stuff

---
+++ tools/scanexr/src/language_provider.rs
@@ -0,0 +1,99 @@
+use std::path::{Path, PathBuf};
+
+use anyhow::Result;
+use async_recursion::async_recursion;
+use async_trait::async_trait;
+use lsp_types::{Location, Url};
+use tree_sitter::{Node, Point, Tree};
+
+use crate::{Convert, Converter};
+
+#[async_trait]
+pub trait LspProvider {
+    async fn find_definitions(&self, location: &Location) -> Result<Vec<Location>>;
+    async fn find_references(&self, location: &Location) -> Result<Vec<Location>>;
+}
+
+pub fn get_node_location(uri: Url, node: &Node) -> Location {
+    Location {
+        uri,
+        range: lsp_types::Range {
+            start: Converter::convert(node.start_position()),
+            end: Converter::convert(node.end_position()),
+        },
+    }
+}
+
+pub fn get_location_node<'a>(root: Node<'a>, location: &Location) -> Option<Node<'a>> {
+    let start = Point {
+        row: location.range.start.line as usize,
+        column: location.range.start.character as usize,
+    };
+    let end = Point {
+        row: location.range.end.line as usize,
+        column: location.range.end.character as usize,
+    };
+
+    root.named_descendant_for_point_range(start, end)
+}
+
+pub fn get_breadcrumbs<'a>(root: Node<'a>, location: &Location) -> Option<Vec<Node<'a>>> {
+    let mut node = get_location_node(root, location)?;
+
+    let mut breadcrumbs = vec![];
+    while let Some(parent_node) = node.parent() {
+        breadcrumbs.push(node);
+        node = parent_node;
+    }
+
+    Some(breadcrumbs)
+}
+
+pub trait LanguageProvider {
+    type State;
+    type LspProvider: LspProvider;
+
+    fn get_tree(&self, location: &Location) -> Result<Tree>;
+
+    fn initial_state(&self) -> Self::State;
+
+    fn get_next_steps(
+        &self,
+        step: &(Location, Self::State),
+        definitions: Result<Vec<Location>>,
+        references: Result<Vec<Location>>,
+    ) -> Result<Vec<(Location, Self::State)>>;
+}
+
+#[async_recursion]
+pub async fn find_paths<L, C, S>(
+    strategy: &S,
+    lsp_provider: &S::LspProvider,
+    start: &(Location, S::State),
+    stop_at: &[Location],
+) -> Result<Vec<Vec<Location>>>
+where
+    S: LanguageProvider + Sync + Send,
+    S::State: Sync + Send,
+    S::LspProvider: Sync,
+{
+    if stop_at.contains(&start.0) {
+        return Ok(vec![vec![start.0.clone()]]);
+    }
+
+    let definitions = lsp_provider.find_definitions(&start.0).await;
+    let references = lsp_provider.find_references(&start.0).await;
+    let next_steps = strategy.get_next_steps(start, definitions, references)?;
+
+    let mut paths = vec![];
+    for next_step in next_steps {
+        let next_paths = find_paths::<L, C, S>(strategy, lsp_provider, &next_step, stop_at).await?;
+
+        for mut next_path in next_paths {
+            next_path.insert(0, start.0.clone());
+            paths.push(next_path);
+        }
+    }
+
+    Ok(paths)
+}
---
+++ tools/scanexr/src/languages/solidity.rs
@@ -0,0 +1,298 @@
+use std::{
+    path::{Path, PathBuf},
+    process::Stdio,
+};
+
+use anyhow::{anyhow, Context, Result};
+use async_trait::async_trait;
+use lsp_client::client::Client;
+use lsp_types::{
+    notification::Initialized,
+    request::{GotoDeclarationParams, GotoDefinition, Initialize},
+    GotoDefinitionResponse, InitializeParams, InitializedParams, Location, PartialResultParams,
+    Range, TextDocumentPositionParams, Url, WorkDoneProgressParams,
+};
+use tokio::{
+    process::{Child, Command},
+    task::JoinHandle,
+};
+use tree_sitter::{Query, Tree};
+
+use crate::{
+    language_provider::{get_breadcrumbs, get_node_location, LanguageProvider, LspProvider},
+    utils::get_query_results,
+    Convert, Converter,
+};
+
+pub struct SolidityLs {
+    client: Client,
+    project_files: Vec<PathBuf>,
+    handles: Vec<JoinHandle<()>>,
+}
+
+impl SolidityLs {
+    fn start_solidity_ls() -> Child {
+        Command::new("solc")
+            .arg("--lsp")
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
+            .expect("failed to start solidity ls")
+    }
+
+    pub async fn new(root_dir: &Path, project_files: Vec<PathBuf>) -> Result<Self> {
+        let (client, handles) = lsp_client::clients::child_client(SolidityLs::start_solidity_ls());
+        client
+            .request::<Initialize>(InitializeParams {
+                root_uri: Some(
+                    Url::from_file_path(root_dir)
+                        .map_err(|_| anyhow!("failed to convert root dir to url"))?,
+                ),
+                ..Default::default()
+            })
+            .await??;
+
+        client.notify::<Initialized>(InitializedParams {})?;
+
+        Ok(Self {
+            client,
+            project_files,
+            handles,
+        })
+    }
+}
+
+impl Drop for SolidityLs {
+    fn drop(&mut self) {
+        for handle in &self.handles {
+            handle.abort();
+        }
+    }
+}
+
+#[async_trait]
+impl LspProvider for SolidityLs {
+    async fn find_definitions(&self, location: &Location) -> Result<Vec<Location>> {
+        let definitions = self
+            .client
+            .request::<GotoDefinition>(GotoDeclarationParams {
+                text_document_position_params: TextDocumentPositionParams {
+                    text_document: lsp_types::TextDocumentIdentifier {
+                        uri: location.uri.clone(),
+                    },
+                    position: location.range.start,
+                },
+                work_done_progress_params: WorkDoneProgressParams {
+                    work_done_token: None,
+                },
+                partial_result_params: PartialResultParams {
+                    partial_result_token: None,
+                },
+            })
+            .await
+            .context("awaiting goto definition response")?
+            .context("getting goto definition result")?;
+
+        if let Some(definitions) = definitions {
+            Ok(match definitions {
+                GotoDefinitionResponse::Scalar(definition) => vec![definition],
+                GotoDefinitionResponse::Array(definitions) => definitions,
+                GotoDefinitionResponse::Link(_) => todo!("what is link?"),
+            })
+        } else {
+            Ok(vec![])
+        }
+    }
+
+    async fn find_references(&self, location: &Location) -> Result<Vec<Location>> {
+        let query = (
+            Query::new(
+                tree_sitter_solidity::language(),
+                "(call_expression function: [
+                (identifier) @ident
+                (member_expression
+                    property: (identifier) @ident
+                )
+            ])",
+            )
+            .unwrap(),
+            0,
+        );
+
+        let fn_call_steps = {
+            let language = tree_sitter_solidity::language();
+
+            let mut locations = vec![];
+            for project_file in &self.project_files {
+                let content =
+                    String::from_utf8(std::fs::read(project_file).context("failed to read file")?)
+                        .context("got non-utf8 file")?;
+
+                let mut parser = tree_sitter::Parser::new();
+                parser
+                    .set_language(language)
+                    .expect("failed to set language on parser");
+                let tree = parser
+                    .parse(&content, None)
+                    .expect("failed to parse content");
+
+                let results = get_query_results(&content, tree.root_node(), &query.0, query.1);
+
+                for result in results {
+                    let start = Converter::convert(result.start_position());
+                    let end = Converter::convert(result.end_position());
+                    let url = Converter::convert(project_file.as_path());
+
+                    locations.push(Location::new(url, Range::new(start, end)));
+                }
+            }
+
+            locations
+        };
+
+        let mut references = vec![];
+        for fn_call_step in fn_call_steps {
+            let Ok(definitions) = self.find_definitions(location).await
+            else {
+                continue;
+            };
+
+            for definition in definitions {
+                if &definition == location {
+                    references.push(fn_call_step.clone());
+                }
+            }
+        }
+
+        Ok(references)
+    }
+}
+
+pub struct Solidity;
+
+#[derive(Debug)]
+pub enum StepMeta {
+    Start,
+    GotoDefinition,
+}
+
+impl LanguageProvider for Solidity {
+    type State = Vec<StepMeta>;
+    type LspProvider = SolidityLs;
+
+    fn initial_state(&self) -> Self::State {
+        vec![StepMeta::Start]
+    }
+
+    fn get_next_steps(
+        &self,
+        step: &(Location, Self::State),
+        definitions: Result<Vec<Location>>,
+        references: Result<Vec<Location>>,
+    ) -> Result<Vec<(Location, Self::State)>> {
+        #[cfg(test)]
+        eprintln!("{}", crate::test_utils::display_location(step));
+
+        let (location, state) = step;
+        let tree = self.get_tree(location)?;
+        let root = tree.root_node();
+
+        let Some(breadcrumbs) = get_breadcrumbs(root, location) else {
+            todo!("when does this happen?")
+        };
+
+        let kinds = breadcrumbs.iter().map(|n| n.kind()).collect::<Vec<_>>();
+
+        #[cfg(test)]
+        dbg!(&kinds);
+
+        match (
+            state.last().unwrap(),
+            kinds.as_slice(),
+            breadcrumbs.as_slice(),
+        ) {
+            (StepMeta::Start, ["identifier", "member_expression", ..], [ident, ..]) => {
+                let location = get_node_location(location.uri.clone(), ident);
+                Ok(vec![(
+                    location,
+                    vec![StepMeta::Start, StepMeta::GotoDefinition],
+                )])
+            }
+            _ => todo!(),
+        }
+    }
+
+    fn get_tree(&self, location: &Location) -> Result<Tree> {
+        let mut parser = tree_sitter::Parser::new();
+        parser
+            .set_language(tree_sitter_solidity::language())
+            .context("failed to set language")?;
+
+        let content =
+            String::from_utf8(std::fs::read(location.uri.to_file_path().unwrap()).unwrap())
+                .unwrap();
+
+        parser.parse(content, None).context("failed to parse text")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test_utils::display_locations;
+    use crate::test_utils::setup_test_dir;
+
+    macro_rules! snapshot {
+        ($input:literal) => {
+            let (_root_dir, location, definitions, references) = setup_test_dir($input);
+
+            let solidity = Solidity;
+
+            let next_steps = solidity
+                .get_next_steps(
+                    &(location, solidity.initial_state()),
+                    Ok(definitions),
+                    Ok(references),
+                )
+                .expect("failed");
+
+            let next_steps = display_locations(next_steps);
+            let snapshot = format!(
+                r#"
+--- input ---
+{}
+
+--- output ---
+{}
+            "#,
+                $input, next_steps
+            );
+
+            insta::assert_snapshot!(snapshot);
+        };
+    }
+
+    #[test]
+    fn test_solidity() {
+        snapshot!(
+            r#"
+contract.sol
+#@#
+contract Contract {
+    function withdraw() public {
+        uint bal = balances[msg.sender];
+        require(bal > 0);
+
+        address target = msg.sender;
+             // ^^^^^^ definition
+
+        (bool sent, ) = target.call{value: bal}("");
+                     // ^^^^^^ start
+        balances[msg.sender] = 0;
+    }
+}
+        "#
+        );
+    }
+}
